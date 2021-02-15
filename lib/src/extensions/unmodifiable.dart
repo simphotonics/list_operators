d6ba7d3edc1505749199d24018d391256118806c
simphotonics <git@simphotonics.com>
2021-02-15 17:05:04 +0000

    Added docs.

--- lib/src/extensions/unmodifiable.dart
+++ lib/src/extensions/unmodifiable.dart
@@ -1,12 +1,15 @@
 import 'dart:collection';
 
+
+/// Extension providing the method `unmodifiable()`.
 extension Unmodifiable<T> on List<T> {
   /// Returns an unmodifiable list view of this.
   UnmodifiableListView<T> unmodifiable() => UnmodifiableListView(this);
 }
 
+/// Extension providing the method `unmodifiable()`.
 extension RecursiveUnmodifiable<T> on List<List<T>> {
-  /// Returns an unmodifiable list view of this.
+  /// Returns an unmodifiable list view of `this`.
   ///
   /// Note: The inner lists are also unmodifiable.
   UnmodifiableListView<UnmodifiableListView<T>> unmodifiable() =>
