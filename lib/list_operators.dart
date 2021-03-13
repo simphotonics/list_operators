330b3b1f18a8fe51e3b042c5ecdc8d364747caba
simphotonics <git@simphotonics.com>
2021-03-13 23:03:00 +0000

    Added export of exception incompatible_lists.

--- lib/list_operators.dart
+++ lib/list_operators.dart
@@ -2,6 +2,7 @@
 /// of type `List<T extends Comparable>`.
 library list_operators;
 
+export 'src/exceptions/incompatible_lists.dart';
 export 'src/extensions/comparison.dart';
 export 'src/extensions/euclidean_metric.dart';
 export 'src/extensions/export.dart';
@@ -9,3 +10,4 @@ export 'src/extensions/double_operators.dart';
 export 'src/extensions/int_operators.dart';
 export 'src/extensions/num_operators.dart';
 export 'src/extensions/unmodifiable.dart';
+export 'src/matchers/ordered_close_to.dart';
