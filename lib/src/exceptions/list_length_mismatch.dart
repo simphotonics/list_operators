1a7acbcfc5701a12f29e4d958f7d80cf7ef22883
simphotonics <git@simphotonics.com>
2021-03-30 12:12:01 +0100

    Renamed incompatible_lists list_length_mismatch.

--- lib/src/exceptions/list_length_mismatch.dart
+++ lib/src/exceptions/list_length_mismatch.dart
@@ -2,4 +2,4 @@ import 'package:exception_templates/exception_templates.dart';
 
 /// Error type indicating that an expression involving list arguments
 /// could not be calculated because the list length does not match.
-class IncompatibleLists extends ErrorType {}
+class ListLengthMismatch extends ErrorType {}
